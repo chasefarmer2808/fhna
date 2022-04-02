2ef91d1b7854dc8fd2d1cc042436481d422c4a2e
cfarmer-fearless <cfarmer@fearless.tech>
2022-04-02 11:11:24 -0400

    bylaw and procedure buttons

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,14 +5,25 @@ import styles from '../../styles/About.module.css';
 import { BoardMemberFrame } from '../../components/BoardMemberFrame';
 import { BoardMember, getBoardMembers } from '../../api/boardMember';
 import { GetStaticProps } from 'next';
+import {
+  FilledLinkToDocumentField,
+  FilledLinkToMediaField,
+} from '@prismicio/types';
+import { Icon } from '../../components/Icon';
 
 interface AboutPagePrismicProps {
   bio: RichTextBlock[];
+  bylawslink: FilledLinkToMediaField;
+  meetingprocedures: FilledLinkToMediaField;
+  financepolicy: FilledLinkToMediaField;
   boardMembers: BoardMember[];
 }
 
 export const About: React.FC<AboutPagePrismicProps> = ({
   bio,
+  bylawslink,
+  meetingprocedures,
+  financepolicy,
   boardMembers,
 }) => {
   return (
@@ -25,12 +36,44 @@ export const About: React.FC<AboutPagePrismicProps> = ({
         <h2>Meet Our Board Members</h2>
         <BoardMemberFrame boardMembers={boardMembers} />
       </div>
+      <div className="content-frame">
+        <h2>Bylaws and Procedures</h2>
+        <section className={styles['link-grid']}>
+          <a
+            className="link-button"
+            target="_blank"
+            rel="noreferrer"
+            href={bylawslink.url}
+          >
+            <span>Bylaws</span>
+            <Icon name="external-link" sizeRem={1.8} />
+          </a>
+          <a
+            className="link-button"
+            target="_blank"
+            rel="noreferrer"
+            href={meetingprocedures.url}
+          >
+            <span>Meeting Procedures</span>
+            <Icon name="external-link" sizeRem={1.8} />
+          </a>
+          <a
+            className="link-button"
+            target="_blank"
+            rel="noreferrer"
+            href={financepolicy.url}
+          >
+            <span>Finance Policy</span>
+            <Icon name="external-link" sizeRem={1.8} />
+          </a>
+        </section>
+      </div>
     </main>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { bio } = (await (
+  const { bio, bylawslink, meetingprocedures, financepolicy } = (await (
     await Client().getByUID('about-page', 'about-page-1', {})
   ).data) as AboutPagePrismicProps;
 
@@ -39,6 +82,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
   return {
     props: {
       bio,
+      bylawslink,
+      meetingprocedures,
+      financepolicy,
       boardMembers,
     },
   };
